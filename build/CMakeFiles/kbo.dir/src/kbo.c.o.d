ba8314899db4a55b383f86eccb2638c96287e07e
Clément Antoine XAVER <clement-antoine.xavier@epitech.eu>
2026-03-09 12:45:01 +0900

    Fix: Remove build dir from git

--- build/CMakeFiles/kbo.dir/src/kbo.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/kbo.dir/src/kbo.c.o: \
-  /Users/clement-antoine-xavier/clement-antoine-xavier/kbo/src/kbo.c \
-  /Users/clement-antoine-xavier/clement-antoine-xavier/kbo/include/kbo/kbo.h
